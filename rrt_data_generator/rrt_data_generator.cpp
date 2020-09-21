f45fa80e9032015f60eac79d46a82cf7b247d9bc
Shelby Ziccardi <shelbyziccardi@gmail.com>
2020-09-21 17:44:14 -0500

    data saved in txt and csv file

--- rrt_data_generator/rrt_data_generator.cpp
+++ rrt_data_generator/rrt_data_generator.cpp
@@ -8,7 +8,8 @@
 
 using namespace std;
 
-ofstream mDataFile;
+ofstream mDataTxtFile;
+ofstream mDataCsvFile;
 vector<string> mHeadings;
 
 float mConfSpaceWidth = 600;
@@ -25,26 +26,35 @@ int mMaxNumSolutionPoints = 300;
 void makeNewRRT() {
 	vec2 randStart = vec2(0.f, rand() % (int)mConfSpaceHeight);
 	vec2 randGoal = vec2(mConfSpaceWidth, rand() % (int)mConfSpaceHeight);
-	mDataFile << randStart.mX << "," << randStart.mY << "," << randGoal.mX << "," << randGoal.mY;
-	if (mNumObstacles > 0) mDataFile << ",";
+	mDataTxtFile << randStart.mX << "," << randStart.mY << "," << randGoal.mX << "," << randGoal.mY;
+	mDataCsvFile << randStart.mX << "," << randStart.mY << "," << randGoal.mX << "," << randGoal.mY;
+	if (mNumObstacles > 0) {
+		mDataTxtFile << ",";
+		mDataCsvFile << ",";
+	}
 	RRT* myRRT = new RRT(mConfSpaceWidth, mConfSpaceHeight, randStart, randGoal, 150);
 
 	for (int i = 0; i < mNumObstacles; i++) {
 		float radius = rand() % (int)(mMaxObstacleRadius - mMinObstacleRadius) + mMinObstacleRadius;
 		vec2 randObstacle = vec2(rand() % (int)(mConfSpaceWidth - 2 * radius) + radius, rand() % (int)(mConfSpaceHeight - 2 * radius) + radius);
-		mDataFile << randObstacle.mX << "," << randObstacle.mY << "," << radius << ",";
+		mDataTxtFile << randObstacle.mX << "," << randObstacle.mY << "," << radius << ",";
+		mDataCsvFile << randObstacle.mX << "," << randObstacle.mY << "," << radius << ",";
 		myRRT->addObstacle(randObstacle, radius);
 	}
 	auto solution = myRRT->start();
-	mDataFile << solution.size() << ",";
+	mDataTxtFile << solution.size() << ",";
+	mDataCsvFile << solution.size() << ",";
 	for (int i = mMaxNumSolutionPoints - 1; i >= 0; i--) {
 		if (i < solution.size()) {
-			mDataFile << solution.at(i).mX << "," << solution.at(i).mY;
+			mDataTxtFile << solution.at(i).mX << "," << solution.at(i).mY;
+			mDataCsvFile << solution.at(i).mX << "," << solution.at(i).mY;
 		} else {
-			mDataFile << randGoal.mX << "," << randGoal.mY;
+			mDataTxtFile << randGoal.mX << "," << randGoal.mY;
+			mDataCsvFile << randGoal.mX << "," << randGoal.mY;
 		}
 		if (i > 0) {
-			mDataFile << ",";
+			mDataTxtFile << ",";
+			mDataCsvFile << ",";
 		}
 	}
 }
@@ -52,7 +62,8 @@ void makeNewRRT() {
 
 int main()
 {
-	mDataFile.open("rrt_data.txt");
+	mDataTxtFile.open("rrt_data.txt");
+	mDataCsvFile.open("rrt_data.csv");
 	//set up headings
 	mHeadings.clear();
 	mHeadings.push_back("\"Start Loc X\"");
@@ -77,17 +88,24 @@ int main()
 	}
 
 	for (int j = 0; j < mHeadings.size(); ++j) {
-		mDataFile << mHeadings.at(j);
-		if (j != mHeadings.size() - 1) mDataFile << ","; // No comma at end of line
+		mDataTxtFile << mHeadings.at(j);
+		mDataTxtFile << mHeadings.at(j);
+		if (j != mHeadings.size() - 1) {
+			mDataCsvFile << ","; // No comma at end of line
+			mDataTxtFile << ","; // No comma at end of line
+		}
 	}
-	mDataFile << endl;
+	mDataCsvFile << endl;
+	mDataTxtFile << endl;
 
 	for (int i = 0; i < mNumDataPoints; i++) {
 		makeNewRRT();
-		mDataFile << endl;
+		mDataCsvFile << endl;
+		mDataTxtFile << endl;
 	}
 	
 
-	mDataFile.close();
+	mDataCsvFile.close();
+	mDataTxtFile.close();
 	return 0;
 }
